0011e1a4a295b3435e557f8ff0756ee78ba99dd5
Cao Viết Huy <hieppro138@gmail.com>
2023-12-11 18:53:52 +0700

    refactor: optimize UI

--- src/App.jsx
+++ src/App.jsx
@@ -22,20 +22,9 @@ const App = () => {
 
   return (
     <>
-      {isOnline ? (
-        <>
-          <Toaster />
-          <Header />
-          <Outlet />
-        </>
-      ) : (
-        <div className="flex flex-col justify-center items-center min-h-screen">
-          <h1 className="text-4xl font-bold">Oops! Connection lost</h1>
-          <p>
-            Looks like you're offline, please check your internet connection.
-          </p>
-        </div>
-      )}
+      <Toaster />
+      <Header />
+      <Outlet />
     </>
   );
 };
